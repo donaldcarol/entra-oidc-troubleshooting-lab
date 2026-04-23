9701ab2fd0aa4f146e181f9e797ca77d356cb7a7
donaldcarol <donald@dakini.network>
2026-04-23 14:18:25 +0300

    initial commit

---
+++ app/server.js
@@ -0,0 +1,17 @@
+{
+  "name": "myfunctions",
+  "version": "1.0.0",
+  "description": "Azure Functions Local Project",
+  "main": "src/functions/*.js",
+  "scripts": {
+    "start": "func start",
+    "test": "echo \"No tests yet...\""
+  },
+  "dependencies": {
+    "@azure/functions": "^4.0.0",
+    "@azure/storage-queue": "^12.29.0"
+  },
+  "devDependencies": {
+    "azure-functions-core-tools": "^4.x"
+  }
+}
